eedfdbe17f3fe656c5ce85621f89e5dee3e57a8f
Fenil Patel <102594553+fbpatel003@users.noreply.github.com>
2022-10-18 22:16:37 +0530

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,84 +1,93 @@
-"use strict";
-let score = 0;
-let randomVar;
-let trialNumber = 1;
-let playMusic = true;
-let buttonVal = document.getElementsByTagName("button");
-let result = document.getElementById("result");
-let report = document.getElementById("text-init-loading");
-let timeS = document.getElementById("timeS");
 let dice = document.getElementById("dice");
+let seconds = document.getElementById("seconds");
+let answer = document.getElementById("answer");
+let allButtons = document.getElementsByTagName("button");
+let score = 0;
+let randNum = -1;
+let hideLoading = document.getElementById("loadingText");
 
-const Clicked = (e) => {
-  let userSelectedNumber = (e.target.innerText);
-  dice.src = `dice${randomVar}.svg`;
-  document.getElementById("text-user-selected").innerHTML = userSelectedNumber;
-  document.getElementById("text-user-selected").style.visibility="visible";
-  if (randomVar == userSelectedNumber) {
-    result.style.color = "#1CE1BE";
-    result.innerHTML = "You guess it right!";
-    score++;
-    document.getElementById("text-total-score").innerHTML = score;
-  } else {
-    result.style.color = "#FF6D2E";
-    result.innerHTML = "Sorry, it was a wrong number!";
-  }
-  Array.from(buttonVal).forEach(element => {
-    element.disabled = true;
-    element.classList.toggle("disabled");
-  });
-  trialNumber++;
-  document.getElementById("text-ttrial-number").innerHTML = trialNumber;
+window.onload = () => {
+  startGame();
+  showTime();
+  NewRandomNum();
 }
 
-const load = () => {
-  report.innerHTML = `<h2>Loading...</h2>
-    <span style="font-size:0.7em;">
-      The game will start in few seconds!
-    </span>`;
-  document.getElementById('h').style.visibility="hidden";
-  result.style.visibility="hidden";
-  document.getElementById("text-user-selected").style.visibility="hidden";
-  Array.from(buttonVal).forEach(element => {
+// game starting and declaration
+const startGame = () => {
+  // hide loading seconds, win/loss & set selected variable to none
+  document.getElementById('secondsLoading').style.visibility="hidden";
+  answer.style.visibility="hidden";
+  document.getElementById("selectedDice").innerHTML=" none";
+
+  // add event on all buttons
+  Array.from(allButtons).forEach(element => {
     element.addEventListener("click", Clicked);
     element.disabled = false;
     element.classList.add("disabled");
   });
+
+  // first game loading screen, game will start in 5 sec
   setTimeout(() => {
-    report.style.visibility="hidden";
-    document.getElementById('h').style.visibility="visible";
-  }, 6000);
+    hideLoading.style.visibility="hidden";
+    document.getElementById('secondsLoading').style.visibility="visible";
+  }, 5000);
 }
 
-const DisplayTime = () => {
-  let timesecond = 9;
+// added event on each buttons
+const Clicked = (e) => {
+  // get randome number and set the picture accoring to that, also set gussed
+  dice.src = `dice${randNum}.svg`;
+
+  // ser user guessed side with selected dice 
+  let guessedSide = (e.target.innerText);
+  document.getElementById("selectedDice").innerHTML = ` ${guessedSide}`;
+  
+  // check if guessed side is correct
+  if (randNum == guessedSide) {
+    answer.style.color = "#00FF00";
+    answer.innerHTML = "Right Guess!!"; // declare win
+    score++;
+    document.getElementById("scoreText").innerHTML = score; // update score++
+  } else { // if it's incorrect
+    answer.style.color = "#FF0000";
+    answer.innerHTML = "Sorry, Wrong Guess!"; // declare loss
+  }
+
+  // disable all the button once one button is clicked
+  Array.from(allButtons).forEach(element => {
+    element.disabled = true;
+    element.classList.toggle("disabled");
+  });
+}
+
+// it will take 10 second to start new round
+const showTime = () => {
+  let maxSeconds = 9;
   setInterval(() => {
-    timeS.innerHTML = timesecond;
-    timesecond--;
-    if (timesecond == 0) {
-      result.style.color = "#FFFFFF";
-      result.innerHTML="Can you guess it?";
-      result.style.visibility="visible";
-      timesecond = 10;
+    seconds.innerHTML = maxSeconds;
+    maxSeconds--;
+    if (maxSeconds == 0) {
+      answer.style.visibility="visible";
+      maxSeconds = 10;
     }
   }, 1000);
 }
 
-const addNewRandom = () => {
+// creating a random number on every 10 seconds
+const NewRandomNum = () => {
   setInterval(() => {
-    randomVar = Math.floor(Math.random() * 6) + 1;
+    randNum = Math.floor(Math.random() * 6) + 1;
+
+    // set rotating dice again for new round
     dice.src = `https://www.animatedimages.org/data/media/710/animated-dice-image-0047.gif`;
     dice.style.width = "37.5%";
-    document.getElementById("text-user-selected").style.visibility="hidden";
-    Array.from(buttonVal).forEach(element => {
+    document.getElementById("selectedDice").innerHTML=" none";
+
+    // enable all the buttons again after 10 seconds
+    Array.from(allButtons).forEach(element => {
       element.disabled = false;
       element.classList.remove("disabled");
     });
   }, 10000);
 }
 
-window.onload = () => {
-  load();
-  DisplayTime();
-  addNewRandom();
-}
